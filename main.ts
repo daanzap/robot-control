eea8d89d6a8591d2840a52c8ff6917a07171194a
Daniel Versteegh <daanzap@users.noreply.github.com>
2021-03-18 10:38:23 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,60 @@
+namespace aiCaptainRobot {
+    let step_time = 200
 
+    function right_forward () {
+        pins.digitalWritePin(DigitalPin.P16, 1)
+    }
+    function turnLeft () {
+        left_forward()
+        right_backward()
+    }
+    function right_backward () {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    }
+    function driveBackwards () {
+        left_backward()
+        right_backward()
+    }
+    function left_backward () {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+    }
+    export function rightOneStep () {
+        turnRight()
+        basic.pause(step_time)
+        all_stop()
+    }
+    export function leftOneStep () {
+        turnLeft()
+        basic.pause(step_time)
+        all_stop()
+    }
+    export function backwardOneStep () {
+        driveBackwards()
+        basic.pause(step_time)
+        all_stop()
+    }
+
+    function turnRight () {
+        right_forward()
+        left_backward()
+    }
+    export function forwardOneStep () {
+        driveForward()
+        basic.pause(step_time)
+        all_stop()
+    }
+    function driveForward () {
+        left_forward()
+        right_forward()
+    }
+    function left_forward () {
+        pins.digitalWritePin(DigitalPin.P12, 1)
+    }
+    function all_stop () {
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+
+}
